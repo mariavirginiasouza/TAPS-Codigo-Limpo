ade8dda8d5d17e716c3e8c3df1e733d285eea1b9
Maria Virginia Souza <mariavirginiasouza02@gmail.com>
2025-05-02 12:43:02 -0300

    Create produto.java

---
+++ produto.java
@@ -0,0 +1,56 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularValorTotal() {
+        return preco * quantidade;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerEstoque(int quantidade) {
+        if (this.quantidade >= quantidade) {
+            this.quantidade -= quantidade;
+        } else {
+            System.out.println("Quantidade insuficiente em estoque.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nome + ", R$ " + preco + ", " + quantidade + " unidades";
+    }
+}
